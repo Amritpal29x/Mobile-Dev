d87e06fd0aef5ee66fc69a8d66591194af7895a6
Amritpal29x <amritplaha2004@gmail.com>
2026-05-09 10:29:36 +0530

    counter app

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,16 +1,17 @@
-import{useState} from "react";
+import {useState} from "react";
 
 function App(){
-    const[state, setState] = useState(0)
-
-
-return(
-<div>
-    <h1>  {count}</h1>
-    <button onClick={setCount(count+1)}>+</button>
-    <button onClick={()=>setCount(count-1)}>-</button>
-    </div>
-);
+ 
+  const [count , setCount] = useState(0)
+   
+ return(
+ 
+ <div>
+ 
+    <h1>{count}</h1>   
+    <button onClick={()=>setCount(count + 1)}>+</button>
+<button onClick={()=>setCount(count - 1)}>-</button>
+</div>
+  );
 }
-
 export default App;
--- react-refresher/src/main.jsx
+++ react-refresher/src/main.jsx
@@ -1,5 +1,23 @@
-import{createRoot} from "react-dom/client"
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App';
 
-createRoot(document.getElementById("root")).render(
-  <h1>Hello World!</h1>
-)
+
+
+//const h1 = React.createElement("h1" , null ,"Hello world")
+
+
+ //createRoot(document.getElementById("root")).render(
+ // <h1>Hello world</h1>
+ //)
+
+ let name = "Amrit"
+
+ createRoot(document.getElementById("root")).render(
+  <>
+  <h1  className="title" id="heading">
+    Welcome to Mobile Dev ! {name}
+  </h1>
+  <App/>
+  </>
+ );
